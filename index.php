cee734e09ab8fc2ce4d170a23348f7fe78de2082
Vladimir Kovpak <cn007b@gmail.com>
2016-12-12 19:39:08 +0200

    Updated test.

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <pre>
 <?php
 
+error_reporting(E_ALL); // error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors','On');
+
 $m = new MongoClient('mongodb://skipeUserAdditional:asdfOYUYKHK57fasdf@ds027771.mongolab.com:27771/skipe');
 $db = $m->selectDB('skipe');
 $collection = new MongoCollection($db, 'user');
