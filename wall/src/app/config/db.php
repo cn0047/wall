f27322b96e80b8bf0618f0f6758ab6936bbaf322
Vladimir Kovpak <cn007b@gmail.com>
2016-12-13 12:04:18 +0200

    Delete db.php

--- wall/src/app/config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return ['mysql' => [
-    'host' => 'localhost',
-    'dbname' => 'id334595_thewalldb',
-    'user' => 'id334595_thewalldbuser',
-    'password' => 'kadklpoercmCER0494lddf24p465678lgkeprk',
-]];
