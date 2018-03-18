dfb66b7293cbb0d949399b3d90f8d79b801d4fd9
Vladimir Kovpak <cn007b@gmail.com>
2018-03-18 22:10:58 +0200

    Updated Event/MessageCreated.

--- wall/src/ddd/Wall/Domain/Model/Message/Event/MessageCreated.php
+++ wall/src/ddd/Wall/Domain/Model/Message/Event/MessageCreated.php
@@ -7,7 +7,15 @@ use Wall\Domain\Model\Message\DTO\Message as MessageDTO;
 
 class MessageCreated implements EventInterface
 {
+    private $dto;
+
     public function __construct(MessageDTO $dto)
     {
+        $this->dto = $dto;
+    }
+
+    public function getDTO(): MessageDTO
+    {
+        return $this->dto;
     }
 }
